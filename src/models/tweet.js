410e13a043edcd3e24f6aaf9b1eaf89253248688
gyanabritish <gyannayak396@gmail.com>
2023-06-25 23:02:43 +0530

    added like and basic user model

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -5,12 +5,7 @@ const tweetSchema = new mongoose.Schema({
         type: String,
         required: true,
         max: [250,'tweet cannot be more than 250 chars']
-    },
-    hashtags :[{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'Hashtag'
-    }, 
-    ] 
+    } 
      
 },{
     timestamps: true
