9aa050bcce3c4523b4947ecc9f1a27f0f659bfdd
gyanabritish <gyannayak396@gmail.com>
2023-06-26 23:52:45 +0530

    added toggle like api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 import { createTweet } from '../../controllers/tweet-controllers.js';
+import { toggleLike } from '../../controllers/like-controller.js';
 
 router.post('/tweets',createTweet);
+
+router.post('/likes/toggle',toggleLike);
 
 
 
